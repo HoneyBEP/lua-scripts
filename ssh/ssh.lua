928afe274aa292240d609a54561564a5e0a12851
Nordin van Nes <N.vanNes@student.tudelft.nl>
2018-05-15 11:28:23 +0200

    Uname command implementation

--- ssh/ssh.lua
+++ ssh/ssh.lua
@@ -3,6 +3,42 @@ function canHandle()
 end
 
 function handle(message)
+
+    local unameCommand = {
+         { -- busybox_w32_32x
+            s = "Windows_NT",
+            o = "MS/Windows",
+            m = "i686",
+            p = "unknown",
+            i = "unknown",
+            M = "unknown",
+            v = "9200",
+            r = "6.2",
+        },
+        { -- busybox_w32_64x
+            s = "Windows_NT",
+            o = "MS/Windows",
+            m = "x86_64",
+            p = "unknown",
+            i = "unknown",
+            M = "unknown",
+            v = "3790",
+            r = "5.2",
+        },
+    }
+
+    if string.match(message, "^uname ") then
+        local uname = unameCommand[math.random(#unameCommand)]
+        local t = {}
+        for p in message:gmatch(" (-%a)") do
+            t[#t + 1] = uname[p:match("%a")]
+        end
+
+        local returnString = table.concat(t, " ")
+
+        return returnString.."\n"
+    end
+
     if message == "uname -n -s -r -v" then
         return "Linux Node-01 4.4.0-116-generic #140-Ubuntu SMP "..timestamp():gsub("^%l", string.upper).."\n"
     end
