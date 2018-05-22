f933535b38ee4d83a0bea723eecb548fdd424d42
Nordin van Nes <N.vanNes@student.tudelft.nl>
2018-05-22 16:25:03 +0200

    Feature for GDDPR http request

--- http/http.lua
+++ http/http.lua
@@ -1,8 +1,31 @@
---
--- Created by IntelliJ IDEA.
--- User: nordi
--- Date: 22-5-2018
--- Time: 15:22
--- To change this template use File | Settings | File Templates.
---
+-- lua script for HTTP exploit
+package.path = './lua-scripts/lua/http/util/?.lua;' .. package.path
+local url = require "url"
 
+function canHandle(message)
+    return true
+end
+
+function handle(message)
+    local questions = {
+        "I consent that all information entered will be stored?",
+        "I consent that all uploads will be stored?",
+        "I consent that all your complete session will be recorded?",
+        "I consent that information will be shared with others?",
+        "I consent that information will be shared with research institutes?",
+        "I consent that information will stored for eternity?",
+    }
+    local html = '<html><head><title>DutchSec - HoneyTrap</title></head><body><script>'
+
+    for key, question in pairs(questions) do
+        html = html .. 'if (confirm("'.. question ..'")) {'
+    end
+
+    for key, question in pairs(questions) do
+        html = html .. '}'
+    end
+
+    html = html .. '</script><h1>Test! Test! Test!</h1></body></html>'
+
+    return html
+end
