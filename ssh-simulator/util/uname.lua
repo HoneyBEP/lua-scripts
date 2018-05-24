95873072ba01e8bd20abe35e3a20a614e6116a9a
Nordin van Nes <N.vanNes@student.tudelft.nl>
2018-05-24 14:01:56 +0200

    Feature SSH Script (#2)
    * Initial SSH script
    
    * Update SSH script
    
    * Uname command implementation
    
    * uname in submodule + submodule usage in main file
    
    * Update SSH Lua to uname
    
    * Fix naming of variable
    
    * Update SSH scripts

---
+++ ssh-simulator/util/uname.lua
@@ -0,0 +1,28 @@
+local uname = {}
+
+function uname.getResponse(message, response)
+    local responseElements = uname.split(response, "##")
+    local responseKeys = {"s", "o", "m", "p", "i", "M", "v", "r"}
+    local uname = {}
+
+    for elementCount = 1, #responseElements do
+        uname[responseKeys[elementCount]] = responseElements[elementCount]
+    end
+
+    local t = {}
+    for p in message:gmatch(" (-%a)") do
+        t[#t + 1] = uname[p:match("%a")]
+    end
+
+    return table.concat(t, " ")
+end
+
+function uname.split(s, delimiter)
+    local result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
+
+return uname
