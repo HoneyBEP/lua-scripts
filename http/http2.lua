1a06157fa02cb995fea42c789d027477534a30fd
Nordin van Nes <N.vanNes@student.tudelft.nl>
2018-05-24 10:50:10 +0200

    Update http2 lua for handle

---
+++ http/http2.lua
@@ -0,0 +1,11 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: nordi
+-- Date: 22-5-2018
+-- Time: 16:52
+-- To change this template use File | Settings | File Templates.
+--
+
+function handle(message)
+
+end
