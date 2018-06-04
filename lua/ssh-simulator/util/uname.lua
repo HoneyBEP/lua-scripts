afe938a36ade2f622b302585f3cca6049bd2162e
Nordin van Nes <N.vanNes@student.tudelft.nl>
2018-06-04 15:47:07 +0200

    Fix typos

--- lua/ssh-simulator/util/uname.lua
+++ lua/ssh-simulator/util/uname.lua
@@ -3,10 +3,10 @@ local uname = {}
 function uname.getResponse(message, response)
     local responseElements = uname.split(response, "##")
     local responseKeys = {"s", "o", "m", "p", "i", "M", "v", "r"}
-    local uname = {}
+    local unameParams = {}
 
     for elementCount = 1, #responseElements do
-        uname[responseKeys[elementCount]] = responseElements[elementCount]
+        unameParams[responseKeys[elementCount]] = responseElements[elementCount]
     end
 
     if message == "uname -a" then
@@ -15,10 +15,10 @@ function uname.getResponse(message, response)
 
     local t = {}
     for p in message:gmatch(" (-%a)") do
-        t[#t + 1] = uname[p:match("%a")]
+        t[#t + 1] = unameParams[p:match("%a")]
     end
 
-    return table.concat(t, " ");
+    return table.concat(t, " ")
 end
 
 function uname.split(s, delimiter)
