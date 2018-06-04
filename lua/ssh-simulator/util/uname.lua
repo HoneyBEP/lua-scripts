811527e9d552d75c962bd027555f10a08143bbdf
Nordin van Nes <N.vanNes@student.tudelft.nl>
2018-06-04 10:23:11 +0200

    Set uname -a

--- lua/ssh-simulator/util/uname.lua
+++ lua/ssh-simulator/util/uname.lua
@@ -9,20 +9,24 @@ function uname.getResponse(message, response)
         uname[responseKeys[elementCount]] = responseElements[elementCount]
     end
 
+    if message == "uname -a" then
+        message = "uname -m -n -r -s -v"
+    end
+
     local t = {}
     for p in message:gmatch(" (-%a)") do
         t[#t + 1] = uname[p:match("%a")]
     end
 
-    return table.concat(t, " ")
+    return table.concat(t, " ");
 end
 
 function uname.split(s, delimiter)
-    local result = {};
+    local result = {}
     for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match);
+        table.insert(result, match)
     end
-    return result;
+    return result
 end
 
 return uname
